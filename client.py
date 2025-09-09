06c14c4b139171e5a22871077719e75f64b67213
HeyItsEquin <theoriginalthemor@gmail.com>
2025-09-09 13:37:31 -0700

    This doesn't work

---
+++ client.py
@@ -0,0 +1,29 @@
+from server import *
+import socket
+import time
+
+class Client:
+    tcp_sock: socket.socket
+    udp_sock: socket.socket
+    s_info: ServerInfo
+
+    def connect(self):
+        self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.tcp_sock.connect((self.s_info.addr, self.s_info.tcp_port))
+        time.sleep(2)
+        self.tcp_sock.close()
+
+
+
+def main():
+    client = Client()
+
+    client.s_info = ServerInfo()
+    client.s_info.addr = "127.0.0.1"
+    client.s_info.tcp_port = 8001
+
+    client.connect()
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,23 +1,45 @@
 from socket import *
+from threading import Thread
 
 class ServerInfo:
     udp_port: int
     tcp_port: int
 
+    addr: str
+
 class Server:
     udp_sock: socket
     tcp_sock: socket
+    tcp_thread: Thread
     info: ServerInfo
 
-    def Start(self):
+    def handle_tcp(self):
         try:
-            self.udp_sock = socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.tcp_sock = socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.tcp_sock.listen()
+            (cl_sock, addr) = self.tcp_sock.accept()
 
-            self.tcp_sock.bind(("127.0.0.1", self.info.tcp_port))
-            self.udp_sock.bind(("127.0.0.1", self.info.udp_port))
+            print("Client connected ( " + addr[0] + ":" + str(addr[1]) + " )")
+        except Exception as e:
+            pass
 
-            print("TCP socket bound")
-            print("UDP socket bound")
+    def setup_sockets(self):
+        self.udp_sock = socket(AF_INET, SOCK_DGRAM)
+        self.tcp_sock = socket(AF_INET, SOCK_STREAM)
+
+        self.tcp_sock.bind((self.info.addr, self.info.tcp_port))
+        print("TCP socket bound")
+
+        tcp_thread = Thread(target=self.handle_tcp)
+        tcp_thread.start()
+        print("Server listening on " + self.info.addr + ":" + str(self.info.tcp_port))
+
+    def start(self):
+        try:
+            self.setup_sockets()
+
+            input()
+        except KeyboardInterrupt:
+            self.tcp_sock.close()
+            if self.tcp_thread: self.tcp_thread.join()
         except Exception as e:
             print("An unhandled exception occurred: \n", e)
