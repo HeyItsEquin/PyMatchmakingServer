e8e7df2b0990269fda47a2c330752ec74aa71601
HeyItsEquin <theoriginalthemor@gmail.com>
2025-09-18 15:12:44 -0700

    Added error handling to Client.

--- network/client.py
+++ network/client.py
@@ -13,54 +13,74 @@ class Client:
         self.id = None
         
     def __cleanup(self):
-        logging.info("Closing client sockets")
+        try:
+            logging.info("Closing client sockets")
 
-        self.tcp.close()
+            self.tcp.close()
+        except Exception as e:
+            logging.error(f"Error occurred during client shutdown: {e}")
 
     def init_tcp_handshake(self):
-        socket.setdefaulttimeout(12.0)
-        
-        msg = Message()
-        msg.header.name = ""
-        msg.header.type = MessageType.CONNECT
-        
-        logging.info("Sending <CONNECT> request, awaiting <SYN> response from server", True)
-        msg.send(self.tcp)
-        
-        buf = recv_all_data(self.tcp)
-        res = Message.from_string(buf)
-        
-        if res.header.type != MessageType.SYN:
-            logging.error(f"Authentication failed. Received <{MessageType(res.header.type).name}> response, expected <SYN>")
-        
-        assigned = UUID(res.body["assigned"])
-        logging.info(f"Received SYN response from servers", True)
-        logging.info(f"Assigned UUID <{assigned}>")
-        logging.info(f"Sending ACK response to server, awaiting <VER> response", True)
-        
-        ack = Message()
-        ack.header.type = MessageType.ACK
-        ack.header.name = ""
-        ack.header.id = assigned
-        
-        ack.send(self.tcp)
-        
-        buf = recv_all_data(self.tcp)
-        res = Message.from_string(buf)
-        if res.header.type == MessageType.VER:
-            logging.info("Ready to communicate")
-        
-        if res.header.type == MessageType.REJ:
-            logging.info("Authentication rejected, likely ID mismatch")
-        
-        self.id = assigned
+        try:
+            socket.setdefaulttimeout(12.0)
+            
+            msg = Message()
+            msg.header.name = ""
+            msg.header.type = MessageType.CONNECT
+            
+            logging.info("Sending <CONNECT> request, awaiting <SYN> response from server", True)
+            msg.send(self.tcp)
+            
+            buf = recv_all_data(self.tcp)
+            res = Message.from_string(buf)
+            
+            if res.header.type != MessageType.SYN:
+                logging.error(f"Authentication failed. Received <{MessageType(res.header.type).name}> response, expected <SYN>")
+            
+            assigned = UUID(res.body["assigned"])
+            logging.info(f"Received SYN response from servers", True)
+            logging.info(f"Assigned UUID <{assigned}>")
+            logging.info(f"Sending ACK response to server, awaiting <VER> response", True)
+            
+            ack = Message()
+            ack.header.type = MessageType.ACK
+            ack.header.name = ""
+            ack.header.id = assigned
+            
+            ack.send(self.tcp)
+            
+            buf = recv_all_data(self.tcp)
+            res = Message.from_string(buf)
+            if res.header.type == MessageType.VER:
+                logging.info("Ready to communicate")
+            
+            if res.header.type == MessageType.REJ:
+                logging.info("Authentication rejected, likely ID mismatch")
+            
+            self.id = assigned
+        except ConnectionRefusedError as e:
+            logging.error("Connection refused. Is the server running?")
+            self.__cleanup()
+        except socket.error as e:
+            logging.error(f"Socket error during TCP handshake: {e}")
+            self.__cleanup()
+        except socket.timeout:
+            logging.error("TCP handshake time out. The server may not be running")
+            self.__cleanup()
+        except Exception as e:
+            logging.error(f"An error occurred during TCP handshake: {e}")
+            self.__cleanup()
         
     def connect(self):
-        self.tcp.connect(("127.0.0.1", 8001))
-        
-        logging.info("Connected to server")
-        logging.info("Initializing TCP handshake")
-        
-        self.init_tcp_handshake()
+        try:
+            self.tcp.connect(("127.0.0.1", 8001))
+            
+            logging.info("Connected to server")
+            logging.info("Initializing TCP handshake")
+            
+            self.init_tcp_handshake()
 
-        self.__cleanup()
+            self.__cleanup()
+        except Exception as e:
+            logging.error(f"Something went wrong when trying to connect to the server: {e}")
+            self.__cleanup()
